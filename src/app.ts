2d0a92d4a2c783f81a8fb5930a7e08c51c12a9d1
Nicolas Thompson <mail@nicolasthompson.com>
2022-08-13 18:51:56 +0100

    Renders map with google map and types

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const addressInput = document.getElementById('address')! as HTMLInputElement;
 const GOOGLE_API_KEY = 'AIzaSyASHRsvIhj_eNLP0de-roo2INCkdJcXYPc';
 
 type GoogleGeocodingResponse = {
-  results: { geometry: { location: { lat: number; lang: number } } }[];
+  results: { geometry: { location: { lat: number; lng: number } } }[];
   status: 'OK' | 'ZERO_RESULTS';
 };
 
@@ -24,6 +24,11 @@ function searchAddressHandler(event: Event) {
         throw new Error('Could not fetch location!');
       }
       const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById('map')!, {
+        center: coordinates,
+        zoom: 16,
+      });
+      new google.maps.Marker({ position: coordinates, map: map });
     })
     .catch((err) => {
       alert(err.message);
