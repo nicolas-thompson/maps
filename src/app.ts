e4ee182d2554917a8475751b2c6a5644cb45ce0d
Nicolas Thompson <mail@nicolasthompson.com>
2022-08-13 18:20:58 +0100

    Fetches coorditates for a given address

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,34 @@
+import axios from 'axios';
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyASHRsvIhj_eNLP0de-roo2INCkdJcXYPc';
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lang: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
-
-  // send this to Google's API!
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== 'OK') {
+        throw new Error('Could not fetch location!');
+      }
+      const coordinates = response.data.results[0].geometry.location;
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
 form.addEventListener('submit', searchAddressHandler);
